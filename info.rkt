90f1e2dfbb5267f1b89ebc693931822f78bf765a
Samuel B. Johnson <sabjonso@yahoo.com>
2020-05-07 20:39:07 -0500

    Added a monad-fail context

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "optional")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "protocol"))
 (define scribblings '(("scribblings/optional.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(sbj))
+(define pkg-desc "Optional Values")
+(define version "0.1")
+(define pkg-authors '("Samuel B. Johnson"))
--- main.rkt
+++ main.rkt
@@ -1,4 +1,26 @@
 #lang racket/base
 
-(require "private/optional.rkt")
-(provide (all-from-out "private/optional.rkt"))
+(require
+ racket/class protocol
+ "private/optional.rkt")
+
+(provide
+ (all-from-out "private/optional.rkt")
+ optional-monad)
+
+(require protocol)
+
+(define OptionalMonad%
+  (class* (send monad-fail instance-base)
+    ((send monad-fail instance-interface))
+    (super-new)
+    (define/override (in-context? x) (optional? x))
+    (define/override (map/f f mx) (optional-map/f f mx))
+    (define/override (return x) (some x))
+    (define/override (map/a mf mx) (optional-map/a mf mx))
+    (define/override (join mmx) (optional-join mmx))
+    (define/override (map/m f mx) (optional-map/m f mx))
+    (define/override (fail) (none))))
+
+(define optional-monad
+  (new OptionalMonad%))
